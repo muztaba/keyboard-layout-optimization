adfbdf919e2710a5d2cc771672f5e2b674fc2382
Muztaba Hasanat <hasanat.muztaba@gmail.com>
2016-10-12 15:14:30 +0600

    model class add

---
+++ src/main/java/com/seal/Key.java
@@ -0,0 +1,16 @@
+package com.seal;
+
+/**
+ * Created by seal on 10/1/2016.
+ */
+public class Key {
+    private final char latter;
+    private final int row;
+    private final int col;
+
+    public Key(char latter, int row, int col) {
+        this.latter = latter;
+        this.row = row;
+        this.col = col;
+    }
+}
---
+++ src/main/java/com/seal/Keyboard.java
@@ -0,0 +1,12 @@
+package com.seal;
+
+import java.util.Set;
+
+/**
+ * Created by seal on 10/1/2016.
+ */
+public class Keyboard {
+    private Set<Key> leftKeys;
+    private Set<Key> rightKeys;
+
+}
