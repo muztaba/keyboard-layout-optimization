b96cf1fee771ffcff7285e7d5867c87deb46bb61
seal <hasanat.muztaba@gmail.com>
2016-10-28 18:14:23 +0600

    check using predicate and add logger

--- src/main/java/com/seal/keyboard/KeyboardFactory.java
+++ src/main/java/com/seal/keyboard/KeyboardFactory.java
@@ -1,14 +1,21 @@
 package com.seal.keyboard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
  * Created by seal on 10/19/2016.
  */
 public class KeyboardFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(KeyboardFactory.class);
+
     private Map<String, Key> keyPosition;
 
     public static Keyboard loadQwert(List<String> list) {
@@ -34,13 +41,20 @@ public class KeyboardFactory {
     public static KeyMap<Character, String> loadKeyMap(List<String > list) {
         Map<Character, String> map = list.stream()
                 .map(i -> i.split(" "))
-                .filter(i -> i.length == 2)
+                .filter(check)
                 .collect(Collectors.toMap(i -> i[0].charAt(0), i -> i[1]));
 
         return new KeyMap<>(map);
     }
 
-
+    private static final Predicate<String[]> check = i -> {
+        if (i.length == 2) {
+            return true;
+        } else {
+            logger.warn("Bad keymap of {}", i[0]);
+            return false;
+        }
+    };
 
     private static Hand hand(String str) {
         switch (str) {
