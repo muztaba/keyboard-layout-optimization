0d1617bc1b963abab7ce4b8de01b54902f4e77ea
seal <hasanat.muztaba@gmail.com>
2017-07-26 15:23:02 +0600

    add method copyOf

--- src/main/java/com/seal/util/CollectionUtil.java
+++ src/main/java/com/seal/util/CollectionUtil.java
@@ -1,7 +1,16 @@
 package com.seal.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by dsi on 7/25/17.
  */
 public class CollectionUtil {
+
+    public <E> List<E> copyOf(List<E> src) {
+        List<E> dest = new ArrayList<>(src.size());
+        src.forEach(i -> dest.add(i));
+        return dest;
+    }
 }
