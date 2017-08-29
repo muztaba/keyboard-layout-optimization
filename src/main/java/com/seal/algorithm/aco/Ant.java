5e2f3e69331c44af31b3fb9256b11187443300f3
frk0000 <frk.tusar@gmail.com>
2017-08-30 03:04:45 +0600

    worked with Ant

--- src/main/java/com/seal/algorithm/aco/Ant.java
+++ src/main/java/com/seal/algorithm/aco/Ant.java
@@ -1,7 +1,10 @@
 package com.seal.algorithm.aco;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.seal.keyboard.KeyMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
 import java.util.stream.DoubleStream;
 
 /**
@@ -9,29 +12,71 @@ import java.util.stream.DoubleStream;
  */
 public class Ant {
 
-    private PheromoneTable pheromoneTable;
+    private static final Logger logger = LoggerFactory.getLogger(Ant.class);
 
-    public void run(String string) {
-        Set<Character> usedBanglaChar = new HashSet<>();
-        Set<String> usedKeyMap = new HashSet<>();
+    private final PheromoneTable pheromoneTable;
+    private final List<Character> banglaChars;
+    private final List<String> charSet;
+
+    public Ant(PheromoneTable pheromoneTable, List<Character> banglaChars, List<String> charSet) {
+        this.pheromoneTable = pheromoneTable;
+        this.banglaChars = banglaChars;
+        this.charSet = charSet;
+    }
+
+    public KeyMap<Character, String> run(String string) {
+        Set<Character> usedBanglaChar = new HashSet<>(banglaChars.size());
+        Set<Integer> usedColumnIndex = new HashSet<>(charSet.size());
+        Map<Character, String> keyMap = new HashMap<>(banglaChars.size());
 
         for (char c : string.toCharArray()) {
             if (!usedBanglaChar.contains(c)) {
-
+                int index = selectKeyMap(c, usedColumnIndex);
+                keyMap.put(c, charSet.get(index));
+                pheromoneTable.evaporate(c, index, .75 /*Should be from config file*/);
+                usedBanglaChar.add(c);
             }
         }
-
+        return new KeyMap<>(keyMap);
     }
 
-    private int selectKeyMap(char c, Set<String> usedKeyMap) {
+    private int selectKeyMap(char c, Set<Integer> usedColumnIndex) {
         double[] array = pheromoneTable.getPheromoneArray(c);
-        int newArrayLen = array.length - usedKeyMap.size();
+        int newArrayLen = array.length - usedColumnIndex.size();
         double[] newArray = new double[newArrayLen];
+        List<Integer> index = new ArrayList<>(newArrayLen);
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!usedColumnIndex.contains(i)) {
+                index.add(i);
+                newArray[j++] = array[i];
+            }
+        }
 
         double sum = DoubleStream.of(newArray)
                 .sum();
 
-        return 0; // should return index
+        PriorityQueue<Node> queue = new PriorityQueue<>(newArrayLen);
+        for (int i = 0; i < newArrayLen; i++) {
+            double probability = newArray[i] / sum;
+            queue.add(new Node(index.get(i), probability));
+        }
+        return queue.poll().index; // should return index
+    }
+
+    private static class Node implements Comparable<Node>{
+        final int index;
+        final double probability;
+
+        public Node(int index, double probability) {
+            this.index = index;
+            this.probability = probability;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Double.compare(o.probability, this.probability);
+        }
     }
 
 }
---
+++ src/main/java/com/seal/algorithm/aco/Runner.java
@@ -0,0 +1,10 @@
+package com.seal.algorithm.aco;
+
+/**
+ * Created by Farruck Ahmed Tusar on 30-Aug-17.
+ */
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
